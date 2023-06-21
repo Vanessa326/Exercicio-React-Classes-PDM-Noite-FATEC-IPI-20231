b432f0914c6ff2707e6d278fc0edbaad14864693
Vanessa Soares <vanis.sbs@gmail.com>
2023-06-21 19:38:18 -0300

    Apostila05(React Classe):Exercício component Class
    -Criado arquivo README.md

--- src/index.js
+++ src/index.js
@@ -1,17 +1,57 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import '@fortawesome/fontawesome-free/css/all.css'
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Card from 'react-bootstrap/Card';
+import Button from 'react-bootstrap/Button';
+import ListGroup from 'react-bootstrap/ListGroup';
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+class App extends React.Component {
+    constructor(props){
+        super(props)
+        this.state={
+            valorSorteado: Array(6).fill(<i className="fas fa-money-bill-1-wave" ></i>)
+        }
+    }
+
+
+    sorteio = () => {
+        
+        const listItems = Array.from({length: 6}, () => Math.floor(Math.random() * 60) + 1);
+
+        this.setState({valorSorteado: listItems})
+
+    }
+
+    render(){
+        return(
+            <Card className="container p-2 mt-2" border="success" as="h5">
+                <Card.Header>MEGA SENA</Card.Header>
+                <Card.Body className="mx-auto" >
+                    <Card.Title >
+                        Números do sorteio
+                    </Card.Title>
+                        <ListGroup horizontal >
+                        {this.state.valorSorteado.map((valor) => <ListGroup.Item variant="success">{valor}</ListGroup.Item>)}
+
+                        </ListGroup>
+
+                    
+                </Card.Body>
+                <Button 
+                        variant="success"
+                        onClick={this.sorteio} 
+                        >
+                        Gerar Jogo!
+                    </Button>
+            </Card>
+        )
+    }
+}
+    
+
+ReactDOM.render(
+    <App />,
+    document.querySelector("#root")
+)
